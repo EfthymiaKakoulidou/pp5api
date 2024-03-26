46e668fdfb6b29cd001b5d3e3c2476530d303774
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-26 13:25:08 +0000

    Add permission to reach_out only when the user is authenticated

--- reach_out/views.py
+++ reach_out/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticated
 from drf_api.permissions import IsReachOutToProfile
 from .models import Reach_out
 from .serializers import Reach_outSerializer, Reach_outDetailSerializer
@@ -6,7 +7,7 @@ from .serializers import Reach_outSerializer, Reach_outDetailSerializer
 
 class Reach_outList(generics.ListCreateAPIView):
     serializer_class = Reach_outSerializer
-    permission_classes = [IsReachOutToProfile]
+    permission_classes = [IsAuthenticated, IsReachOutToProfile]
 
     def get_queryset(self):
         return Reach_out.objects.filter(reach_out_to=self.request.user.profile)
@@ -16,6 +17,6 @@ class Reach_outList(generics.ListCreateAPIView):
 
 
 class Reach_outDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsReachOutToProfile]
+    permission_classes = [IsAuthenticated, IsReachOutToProfile]
     serializer_class = Reach_outDetailSerializer
     queryset = Reach_out.objects.all()
