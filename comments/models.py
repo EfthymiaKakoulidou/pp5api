9782a02f8d291ffa76a2eeeb021dcc6d0bef9c06
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-06 21:03:19 +0000

    Update the seecret field in the comment model to seecretid

--- comments/models.py
+++ comments/models.py
@@ -8,7 +8,7 @@ class Comment(models.Model):
     Comment model, related to User and Seecret
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    seecret = models.ForeignKey(Seecret, on_delete=models.CASCADE, blank=False, null=False)
+    seecretid = models.ForeignKey(Seecret, on_delete=models.CASCADE, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
