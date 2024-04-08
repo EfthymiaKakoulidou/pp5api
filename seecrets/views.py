7a809a2e47cd3e31edcceb8c1cbeb3b5cdb6ab13
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-08 10:23:38 +0000

    Update the seecrets views to include the hugs and comments count

--- seecrets/views.py
+++ seecrets/views.py
@@ -1,20 +1,18 @@
 from django.db.models import Count
-from django.http import Http404
+from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status, permissions, filters
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Seecret
 from .serializers import SeecretSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class SeecretList(APIView):
+class SeecretList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = SeecretSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
-
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Seecret.objects.annotate(
         hugs_count=Count('hugs', distinct=True),
         comments_count=Count('comment', distinct=True)
@@ -25,6 +23,7 @@ class SeecretList(APIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
+        
         'hugs__owner__profile',
         'owner__profile',
     ]
@@ -42,61 +41,13 @@ class SeecretList(APIView):
         serializer.save(owner=self.request.user)
 
 
-    def get(self, request):
-        seecrets = Seecret.objects.all()
-        serializer = SeecretSerializer(
-            seecrets, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = SeecretSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-
-class SeecretDetail(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
+class SeecretDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a seecret and edit or delete it if you own it.
+    """
     serializer_class = SeecretSerializer
-
-    def get_object(self, pk):
-        try:
-            seecret = Seecret.objects.get(pk=pk)
-            self.check_object_permissions(self.request, seecret)
-            return seecret
-        except Seecret.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        seecret = self.get_object(pk)
-        serializer = SeecretSerializer(
-            seecret, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        seecret = self.get_object(pk)
-        serializer = SeecretSerializer(
-            seecret, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        seecret = self.get_object(pk)
-        seecret.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Seecret.objects.annotate(
+        hugs_count=Count('hugs', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
