3a6ac94f06dba33162019dd8c30fa120fcce20dc
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-09 18:57:19 +0000

    Add IsOwner authenircation to reach_out views

--- reach_out/views.py
+++ reach_out/views.py
@@ -1,13 +1,13 @@
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
-from drf_api.permissions import IsReachOutToProfile
+from drf_api.permissions import IsReachOutToProfile, IsOwner
 from .models import Reach_out
 from .serializers import Reach_outSerializer, Reach_outDetailSerializer
 
 
 class Reach_outList(generics.ListCreateAPIView):
     serializer_class = Reach_outSerializer
-    permission_classes = [IsAuthenticated, IsReachOutToProfile]
+    permission_classes = [IsAuthenticated, IsOwner, IsReachOutToProfile]
 
     def get_queryset(self):
         return Reach_out.objects.filter(reach_out_to=self.request.user.profile)
@@ -17,6 +17,6 @@ class Reach_outList(generics.ListCreateAPIView):
 
 
 class Reach_outDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsReachOutToProfile]
+    permission_classes = [IsOwner, IsReachOutToProfile]
     serializer_class = Reach_outDetailSerializer
     queryset = Reach_out.objects.all()
