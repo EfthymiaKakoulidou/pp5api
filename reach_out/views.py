e27b078844a5e8fc0f8bd5c7c089f3d945c242d7
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-10 06:33:21 +0000

    Update the permissions and queryset of reach_out_views to get just the onwer and reach_out_to

--- reach_out/views.py
+++ reach_out/views.py
@@ -1,22 +1,21 @@
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
-from drf_api.permissions import IsReachOutToProfile, IsOwner
 from .models import Reach_out
 from .serializers import Reach_outSerializer, Reach_outDetailSerializer
-
+from drf_api.permissions import IsOwnerOrReadOnly
 
 class Reach_outList(generics.ListCreateAPIView):
     serializer_class = Reach_outSerializer
-    permission_classes = [IsAuthenticated, IsOwner, IsReachOutToProfile]
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
-        return Reach_out.objects.filter(reach_out_to=self.request.user.profile)
+        user = self.request.user
+        return Reach_out.objects.filter(owner=user) | Reach_out.objects.filter(reach_out_to=user.profile)
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
+        serializer.save(owner=self.request.user.profile)
 
 class Reach_outDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsOwner, IsReachOutToProfile]
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = Reach_outDetailSerializer
     queryset = Reach_out.objects.all()
