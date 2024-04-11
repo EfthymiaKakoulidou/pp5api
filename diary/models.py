59bc0493116dd5a73e6261e496aedf125ca07d92
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-11 21:13:45 +0000

    Make content field requires at diary model

--- diary/models.py
+++ diary/models.py
@@ -19,7 +19,7 @@ class Diary(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=False)
 
     class Meta:
         ordering = ['-created_at']
