c32daa01c5a6cf1020bd0d00c67311f7a30d7677
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-13 08:43:04 +0000

    Update the profilelist view in profiles

--- profiles/views.py
+++ profiles/views.py
@@ -7,21 +7,22 @@ from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 
-class ProfileList(APIView):
-    permission_classes = [IsAuthenticated]
+class ProfileList(generics.ListAPIView):
     """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
+    List all profiles.
+    No create view as profile creation is handled by django signals.
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request
-            })
-        return Response(serializer.data)
-
+    queryset = Profile.objects.all()
+    
+    serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you're the owner.
