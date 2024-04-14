330e0137fe01b846834f79000a9fa154d65fe934
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-14 15:36:44 +0000

    Update permission of reach_out detail to authenticated owner or reachoutto profile

--- reach_out/views.py
+++ reach_out/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
 from .models import Reach_out
 from .serializers import Reach_outSerializer, Reach_outDetailSerializer
-from drf_api.permissions import IsOwnerOrReadOnly, IsReachOutToProfile
+from drf_api.permissions import IsOwnerOrReadOnly, IsReachOutToProfile, IsOwner
 
 class Reach_outList(generics.ListCreateAPIView):
     serializer_class = Reach_outSerializer
@@ -16,6 +16,6 @@ class Reach_outList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class Reach_outDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly, IsReachOutToProfile]
+    permission_classes = [IsAuthenticated, IsOwner, IsReachOutToProfile]
     serializer_class = Reach_outDetailSerializer
     queryset = Reach_out.objects.all()
