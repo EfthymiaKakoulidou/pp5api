d4e1cdbe5c2d1b2d0fd08a02188fa9025cf9d90a
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-18 06:22:38 +0000

    Fix line too long in reach_out models

--- reach_out/models.py
+++ reach_out/models.py
@@ -8,7 +8,12 @@ class Reach_out(models.Model):
     Reach_out model, related to User and Profile
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    reach_out_to = models.ForeignKey(Profile, on_delete=models.CASCADE, null=False, blank=False)
+    reach_out_to = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField(blank=False)
